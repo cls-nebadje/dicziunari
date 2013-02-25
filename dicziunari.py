9a0618eb7f62e9259366d26a6f86ad816d088f32
Uli Franke <uli.franke@weiss.ch>
2013-02-25 17:40:38 +0100

    * Removed module stuff - seems that parsing became simpler than thought  * Converted parser stuff to class  * Added addition control sequence
    Test with Puter.lzm ran through without any problems

--- dicziunari.py
+++ dicziunari.py
@@ -9,109 +9,17 @@
 #print "\x9f\x8a\x9a".decode("mac-roman")
 
 import collections, os, sys
+from lzmparser import Parser
 
 
-def createRef(pattern):
-    ref = collections.deque([i for i in pattern], len(pattern))
-    x = collections.deque(["\x00" for _ in range(len(ref))], len(ref))
-    return ref, x
-    
-def findPattern(data, pattern, callback):
-    ref, x = createRef(pattern)
-    P = len(ref)
-    pos = 0
-    first = ref[0]
-    L = len(data)
-    while pos < L:
-        if first not in x:
-            x.extend(data[pos:pos+P])
-            pos += P
-        else:
-            x.append(data[pos])
-            pos += 1
-        if x == ref:
-            callback(data, pos)
-
-f = open("Vallader.lzm", "r")
-data = f.read()
-f.close()
-
-def anlz_01ff05():
-    # row
-    # 01 FF 05 00 00 00 00 3E C0 C0
-    e = {}
-    
-    def cb(d, p):
-        t = d[p]
-        count, _ = e.get(t, (0, 0))
-        e[t] = count + 1, p
-    
-    findPattern(data, "\x01\xFF\x05\x00\x00\x00\x00", cb)
-
-    print '%8s   %6s    %8s' % ("next chr", "count", "lastaddr")
-    print '-' * 80
-    items = e.items()
-    items.sort(key=lambda i : i[1][0], reverse=True)
-    for key, (count, last) in items:
-        res = '%8s : %6i  @ %8x' % (repr(key), count, last)
-        print res
-
- 
-def analyze_xxx():
-    
-    e = {}
-    
-    def cb(d, p):
-        t = d[p-5]
-        count, _ = e.get(t, (0, 0))
-        e[t] = count + 1, p
-    
-    findPattern(data, "\x03xxx", cb)
-    
-    print '%8s   %6s    %8s   %s' % ("next chr", "count", "lastaddr", "contents")
-    print '-' * 80
-    items = e.items()
-    items.sort(key=lambda i : i[1][0], reverse=True)
-    for key, (count, last) in items:
-        res = '%8s : %6i  @ %8x : "%s"' % (repr(key), count, last, repr(data[last:last+10]))
-        print res
-
-def rnd(value, multiple):
-    return (value + multiple - 1) & (-multiple)
-
-def read0x200():
-    import struct, parse.page
-    L = len(data)
-    pos = 0x1000
-    pageSize = 0x400-0x10
-    parser = parse.page.Parser()
-    while pos < L:
-        # type |      |      |      |      |      | size | table |
-        # 00 00 00 00 | 00 5E 00 00 | 00 D2 00 09 | 00 59 C1 11 |  
-        etype, _, _, _, _, _, esize, table = struct.unpack_from(">HHHHHHHH", data, pos)
-        print "@ %8x : %4i %4x" % (pos, esize, table)
-        
-        pos += 0x10
-        # Problem with esize at entry at 0x87c00 which is somewhat larger
-#        pagedata = data[pos:pos+esize]
-        pagedata = data[pos:pos+pageSize]
-        
-        if table == 0xc105:
-            parser.dicziunari(pagedata, etype)
-        
-        pos = rnd(pos + esize, 0x400)
+def parse():
+    parser = Parser("Vallader.lzm")
+    parser.parseFile()
         
     print "Entries:   ", len(parser.entries)
     print "Collisions:", parser.collisions
-        
-read0x200()
-
-#def anlz_hbam():
-#    "HBAM3016AUG95"
-# bis 0x1000
-
-#anlz_01ff05()
-
-#analyze_xxx()
+    
+    # http://www.blog.pythonlibrary.org/2012/07/18/python-a-simple-step-by-step-sqlite-tutorial/
+parse()
 
 
---
+++ lzmparser.py
@@ -0,0 +1,121 @@
+
+import struct, time, sys
+
+def rnd(value, multiple):
+    return (value + multiple - 1) & (-multiple)
+
+# 0x6d7b0 : "Bezugsdatensaetze markieren mit xxx"
+
+class Parser:
+    pageSize = 0x400-0x10
+    dictionariPageCode = 0xc105
+    
+    def __init__(self, filePath):
+        f = open("Vallader.lzm", "r")
+        self.fileData = f.read()
+        f.close()
+        self.entries = {}
+        self.collisions = []
+        
+    def parseFile(self):
+        L = len(self.fileData)
+        pos = 0x1000
+        while pos < L:
+            # type |      |      |      |      |      | size | table |
+            # 00 00 00 00 | 00 5E 00 00 | 00 D2 00 09 | 00 59 C1 11 |  
+            etype, _, _, _, _, _, esize, table = struct.unpack_from(">HHHHHHHH", self.fileData, pos)
+            print "@ %8x : %4i %4x" % (pos, esize, table)
+            
+            pos += 0x10
+            # Problem with esize at entry at 0x87c00 which is somewhat larger
+    #        pagedata = data[pos:pos+esize]
+            pagedata = self.fileData[pos:pos+self.pageSize]
+            
+            if table == self.dictionariPageCode:
+                self.parsePage(pagedata, etype, pos-0x10)
+            
+#            pos = rnd(pos + esize, 0x400)
+            pos += self.pageSize
+    
+    def parsePage(self, data, etype, pageAddress):
+        
+        if etype != 0x0000:
+            print "Unknown table entry type: %x" % etype
+            return
+
+        pos = 0
+        L = len(data)
+        
+        # At least 4 bytes are required to read the row ID
+        while pos < L-4 and 0xC0 != ord(data[pos]):
+            rowId, = struct.unpack_from(">I", data, pos)
+            pos += 0x04
+            
+            entry = self.entries.get(rowId, {})
+            self.entries[rowId] = entry
+            entry["address"] = pageAddress
+            
+            while pos < L:
+                col, = struct.unpack_from(">B", data, pos)
+                pos += 1
+                
+                if col == 0x00: # hack because of page data length problem
+                    pos = L
+                    break
+                
+                if col == 0xC0:
+                    if pos < L:
+                        tmp, = struct.unpack_from(">B", data, pos)
+                        if tmp == 0xc0 or tmp == 0x00:
+                            # end of entry (to be verified!)
+                            pos = L
+                    break
+                
+                elif col == 0xC1:   # Special control sequences
+                    # Encountered
+                    # 0x0006d800 0xc1 0x0b
+                    # 0x00071400 0xc1 0x0b 0xc0
+                    # 0x0007dc00 0xc1 0x0b 0xc2
+                    # 0x00815000 0xc1 0x0f
+                    # 0x00334010 0xc1 0x0f 0xc2
+                    # 0x0098a000 0xc1 0x05 0xc0
+                    # 0x017d5c00 0xc1 0x12 0xc0
+                    a, b = struct.unpack_from(">BB", data, pos)
+                    if a in [0x0b, 0x05, 0x0f, 0x12]:
+                        if b == 0xc0:
+                            pos += 0x02
+                        elif b == 0xc2: # Unknown data block following (for examples see above)
+                            pos = L
+                            break
+                        else:
+                            pos += 0x01
+                    continue
+                
+                if col == 0x01: # Those magic 01ff05 blocks which seem to have a constant length of 89
+                    ff, five = struct.unpack_from(">BB", data, pos)
+                    if ff == 0xff and five == 0x05:
+                        if 0:
+                            pos += 89 - 1 # -1 because we already read one
+                        else:
+                            npos = data.find("\xc0\xc0", pos)
+                            if npos == -1:  # probably end of page
+                                pos = L
+                            else:
+                                pos = npos + 2
+                        continue
+    
+                clen, = struct.unpack_from(">B", data, pos)            
+                pos += 1
+                
+                content = data[pos:pos+clen]
+                pos += clen
+    
+                content = content.decode('mac-roman')
+                col = chr(col).decode('mac-roman')
+                    
+                if col in entry:
+                    if content not in "xxx":
+                        self.collisions.append([rowId, col, [content, entry[col]]])
+                        print "Entry collision: %x %s %s" % (rowId, col, content)
+                entry[col] = content
+
