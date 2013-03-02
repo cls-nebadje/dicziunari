f3348158bc2cc9b1f7a64c10ff4730846ef8c1fe
Uli Franke <cls@nebadje.org>
2013-03-02 03:26:25 +0100

    * Added raw viewing mode to inspect data base rows in raw mode  * Some (dirty) column documentation

--- dicziunari.py
+++ dicziunari.py
@@ -16,6 +16,7 @@ def main():
     anaSqlGrp.add_argument("-a", "--analisar", action='store_true', help="be analisa la veglia banca da datas")
     anaSqlGrp.add_argument("-s", "--sqlite", action='store_true', help="crea SQLite banca da datas")
     anaSqlGrp.add_argument("-t", "--tscherchar", help="tschercha alch aint illa nouva banca da datas")
+    anaSqlGrp.add_argument("-l", "--lingias", help="tschercha alch aint illa nouva banca da datas ma muossa las crüjas lingias")
     args = ap.parse_args()
     
     if args.puter:
@@ -35,6 +36,8 @@ def main():
         
     if args.tscherchar:
         query(dbPath, args.tscherchar)
+    if len(args.lingias) > 0:
+        query(dbPath, args.lingias, raw=True)
 
 def parse(dicziunariPath):
     parser = Parser(dicziunariPath)
@@ -47,17 +50,29 @@ def parse(dicziunariPath):
     print "Columns:   ", len(cols), ", ".join(["'%s'" % c for c in cols])
     return parser
 
-def query(sqliteDbPath, query):
+def query(sqliteDbPath, query, raw=False):
     if not os.path.exists(sqliteDbPath):
         print >> sys.stderr, "%s not found. Refer to -h for further information."
     conn = sqlite3.connect(sqliteDbPath)
     cursor = conn.cursor()
     
-    sql = "SELECT m, n FROM dicziunari WHERE m LIKE '%%%s%%' OR n LIKE '%%%s%%'" % (query, query)
+    if raw:
+        cursor.execute("PRAGMA table_info(dicziunari)")
+        cols = cursor.fetchall()
+        sql = "SELECT * FROM dicziunari WHERE m LIKE '%%%s%%' OR n LIKE '%%%s%%'" % (query, query)
+    else:
+        sql = "SELECT m, n FROM dicziunari WHERE m LIKE '%%%s%%' OR n LIKE '%%%s%%'" % (query, query)
     cursor.execute(sql)
     res = cursor.fetchall()
-    for m, n, in res:
-        print "  %35s : %-35s" % (m, n)
+    if raw:
+        for row in res:
+            for i, col in enumerate(row):
+                print "%s : %s" % (cols[i][1], col)
+            print "-" * 80
+    else:
+        for m, n, in res:
+            print "  %35s : %-35s" % (m, n)
+        
 
 def createSQLite(sqliteDbPath, parser, tableName = "dicziunari"):
     # http://www.blog.pythonlibrary.org/2012/07/18/python-a-simple-step-by-step-sqlite-tutorial/
@@ -71,35 +86,35 @@ def createSQLite(sqliteDbPath, parser, tableName = "dicziunari"):
     cols = parser.getColumns()
     C = len(cols)
     colMap = {'address':'address',
-              'A': 'aa', # vallader
-              'C': 'cc', # puter
-              'B': 'bb',
+              'A': 'aa', # vallader only
+              'C': 'cc', # puter only
+              'B': 'bb',    # Stichwort key (rumantsch)
               'E': 'ee',
               'D': 'dd',
               'G': 'gg',
-              'I': 'ii',
-              'H': 'hh',
+              'I': 'ii',    # bereich (rumantsch)
+              'H': 'hh',    # a), 2. (rumantsch)
               'K': 'kk',
               'J': 'jj',
-              'L': 'll',
+              'L': 'll',    # gener (rumantsch)
               'O': 'oo',
               'N': 'nn',
               'Q': 'qq',
               'P': 'pp',
               'S': 'ss',
-              'R': 'rr',
-              'T': 'tt',
-              'W': 'ww',
-              'V': 'vv',
+              'R': 'rr',    # bereich (tudais-ch)
+              'T': 'tt',    # plural/variazionen/deklinationen
+              'W': 'ww',    # gener (tudais-ch)
+              'V': 'vv',    # Stichwort key Deutsch
               'X': 'xx',
-              '_': 'us',
-              '^': 'zf',
+              '_': 'us',    # Wortgruppe Rumantsch
+              '^': 'zf',    # Wortgruppe Deutsch
               'e': 'e',
               'd': 'd',
-              'k': 'k',
-              'j': 'j',
-              'm': 'm',
-              'n': 'n',
+              'k': 'k',     # Duplicat Rumantsch (ma perche?)
+              'j': 'j',     # Duplicat Tudais-ch (ma perche?)
+              'm': 'm',     # Tudais-ch
+              'n': 'n',     # Rumantsch
               }
 
     
